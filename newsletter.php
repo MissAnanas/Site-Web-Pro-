f00fd401bca251cd450d6142c9418750234c8165
Giron Willem <147034330+WSkaro@users.noreply.github.com>
2024-02-09 04:59:18 +0100

    update

---
+++ newsletter.php
@@ -0,0 +1,9 @@
+<div class="newsletter">
+    <div class="newsletter_content">
+        <h1 class="titre_newsletter">Souscrivez à notre <font color="#FFDC60">newsletter</font></h1>
+        <div class="input_content">
+            <input class="input_mail" type="text" name="mail" placeholder="Entrez votre E-Mail..." >
+            <button class="btn_souscrire">Souscrire</button>
+        </div>
+    </div>
+</div>
